f36aa7d96b40dccd743a744b5b4cb5e66f871e67
ma1088 <ma1088@live.com>
2022-09-08 22:26:07 -0300

    integração com front-end

--- src/main/java/com/marilia/pa/pautabackend/controller/PautaController.java
+++ src/main/java/com/marilia/pa/pautabackend/controller/PautaController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import com.marilia.pa.pautabackend.dto.PautaDTO;
 import com.marilia.pa.pautabackend.service.PautaService;
 
 @Controller
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/")
 public class PautaController {
     private final Logger log = Logger.getLogger(this.getClass().getName());
