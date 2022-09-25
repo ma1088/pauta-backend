6620b4b885c9ec4ac78cc2e9c3135088bbaadd9d
ma1088 <ma1088@live.com>
2022-09-24 23:37:44 -0300

    Close #124: ajustes

--- src/main/java/com/marilia/pa/pautabackend/model/Pauta.java
+++ src/main/java/com/marilia/pa/pautabackend/model/Pauta.java
@@ -14,6 +14,7 @@ import javax.persistence.Table;
 public class Pauta {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idPauta;
     @Column
     private String autor;
